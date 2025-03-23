00ce38b4d05ccbd51440c72490b819ddfa785f22
Matthieu Scarset <m@matthieuscarset.com>
2025-03-23 17:04:16 +0100

    fetch events

--- script/fetch_drupal_data.sh
+++ script/fetch_drupal_data.sh
@@ -7,7 +7,6 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 OUTPUT_FOLDER="$SCRIPT_DIR/../data/json"
 BASE_URL="https://www.drupal.org/api-d7"
-COLUMNS_TO_KEEP='.title, .nid, .url, .field_budget, .field_organization_headquarters'
 
 mkdir -p "$OUTPUT_FOLDER"
 
@@ -55,22 +54,22 @@ case "$1" in
         fetch_data "user" "user.json" "sort=uid&direction=ASC" '{
             id: .uid,
             title: .name,
-            fname: .field_first_name,
-            lname: .field_last_name,
+            fname: (.field_first_name // null),
+            lname: (.field_last_name // null),
             created: .created,
-            da_membership: .field_da_ind_membership,
-            slack: (if .field_slack == [] then null else .field_slack end),
-            mentors: ([.field_mentors[].id | tostring] | join(",") | split(",")),
-            countries: (if .field_country == [] then null else .field_country end),
-            language: .field_user_primary_language,
-            languages: (if .field_languages == [] then null else .field_languages end),
-            timezone: .timezone,
+            da_membership: (.field_da_ind_membership // null),
+            slack: (.field_slack // null),
+            mentors: ([.field_mentors[]?.id | tostring] // [] | join(",") | split(",")),
+            countries: (.field_country // null),
+            language: (.field_user_primary_language // null),
+            languages: (.field_languages // []),
+            timezone: (.timezone // null),
             region: (if .timezone | length > 0 then (.timezone | tostring | split("/") | first) else null end),
             city: (if .timezone | length > 0 then (.timezone | tostring | split("/") | last) else null end),
-            organizations: ([.field_organizations[].id | tostring] | join(",") | split(",")),
-            industries: .field_industries,
-            contributions: .field_contributed,
-            events: .field_events_attended
+            organizations: ([.field_organizations[]?.id | tostring] // [] | join(",") | split(",")),
+            industries: (.field_industries // null),
+            contributions: (.field_contributed // null),
+            events: (.field_events_attended // null),
         }'
         ;;
     organization)
@@ -79,13 +78,31 @@ case "$1" in
             title: .title,
             created: .created,
             changed: .changed,
-            author: .author.id,
-            url: (if (.field_link | type) == "array" then (.field_link[]?.url // null) elif (.field_link | type) == "object" then .field_link.url else null end),
-            budget: (if .field_budget == [] then null else .field_budget end),
-            headquarters: (if .field_organization_headquarters == [] then null else .field_organization_headquarters end)
+            author: (.author.id // null),
+            url: (.field_link.url // null),
+            budget: (.field_budget // null),
+            headquarters: (.field_organization_headquarters // null),
+        }'
+        ;;
+    event)
+        fetch_data "event" "node.json" "type=event&sort=nid&direction=ASC" '{
+            id: .nid,
+            title: .title,
+            from: (.field_date_of_event.value // null),
+            to: (.field_date_of_event.value2 // null),
+            duration: (.field_date_of_event.duration // null),
+            event_type: (.field_event_type // [] | join("")),
+            event_format: (.field_event_format // [] | join("")),
+            author: (.author.id // null),
+            speakers: (.field_event_speakers // [] | map(.id | tostring)),
+            sponsors: (.field_event_sponsors // [] | map(.id | tostring)),
+            volunteers: (.field_event_volunteers // [] | map(.id | tostring)),
+            organizers: (.field_organizers // [] | map(.id | tostring)),
+            city: (if (.field_event_address | type == "object") then .field_event_address.locality else null end),
+            country: (if (.field_event_address | type == "object") then .field_event_address.country else null end),
         }'
         ;;
     *)
-        echo "Usage: $0 {user|organization}"  exit 1
+        echo "Usage: $0 {user|organization|event}"  exit 1
         exit 1
         ;;esac
