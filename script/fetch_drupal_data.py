6fc6606377b203c12fbb229f1cb8ce9bd43271a3
Matthieu Scarset <m@matthieuscarset.com>
2025-03-30 22:39:59 +0200

    wip

--- script/fetch_drupal_data.py
+++ script/fetch_drupal_data.py
@@ -69,15 +69,15 @@ def get_mapping(name):
         "from": "(.field_date_of_event.value // null)",
         "to": "(.field_date_of_event.value2 // null)",
         "duration": "(.field_date_of_event.duration // null)",
-        "event_type": "(.field_event_type // [] | join(''))",
-        "event_format": "(.field_event_format // [] | join(''))",
+        "event_type": "(.field_event_type // [] | join(\"\"))",
+        "event_format": "(.field_event_format // [] | join(\"\"))",
         "author": "(.author.id // null)",
-        "speakers": "(if (.field_event_speakers | type == 'array') then .field_event_speakers | map(.id | tostring) else [] end)",
-        "sponsors": "(if (.field_event_sponsors | type == 'array') then .field_event_sponsors | map(.id | tostring) else [] end)",
-        "volunteers": "(if (.field_event_volunteers | type == 'array') then .field_event_volunteers | map(.id | tostring) else [] end)",
-        "organizers": "(if (.field_organizers | type == 'array') then .field_organizers | map(.id | tostring) else [] end)",
-        "city": "(if (.field_event_address | type == 'object') then .field_event_address.locality else null end)",
-        "country": "(if (.field_event_address | type == 'object') then .field_event_address.country else null end)"
+        "speakers": "(if (.field_event_speakers | type == \"array\") then .field_event_speakers | map(.id | tostring) else [] end)",
+        "sponsors": "(if (.field_event_sponsors | type == \"array\") then .field_event_sponsors | map(.id | tostring) else [] end)",
+        "volunteers": "(if (.field_event_volunteers | type == \"array\") then .field_event_volunteers | map(.id | tostring) else [] end)",
+        "organizers": "(if (.field_organizers | type == \"array\") then .field_organizers | map(.id | tostring) else [] end)",
+        "city": "(if (.field_event_address | type == \"object\") then .field_event_address.locality else null end)",
+        "country": "(if (.field_event_address | type == \"object\") then .field_event_address.country else null end)"
     }
 
     taxonomy_terms_mapping = {
@@ -100,9 +100,10 @@ def get_mapping(name):
         "organization": organization_mapping,
         "module": module_mapping,
         "event": event_mapping,
-        "taxonomy_terms": taxonomy_terms_mapping,
+        "module_terms": taxonomy_terms_mapping,
         "theme": theme_mapping
     }
+
     return mappings.get(name, None)
 
 
@@ -118,7 +119,16 @@ def get_total_pages(url, params):
         int: The total number of pages.
     """
     try:
-        # Request full 0 to get the last page.
+        # Request with full:0 and limit:1 to get last page number, if any.
+        data = requests.get(url, headers=HEADERS, params={
+                            **params, 'full': 0, 'limit': 1}).json()
+        # Extract the total number of pages from the last page URL.
+        total_pages = int(data['last'].split('page=')[1])
+        if total_pages == 0:
+            return 0
+
+        # Now we know there is at least one page of result.
+        # Get total pages with no limit to get real last page number.
         data = requests.get(url, headers=HEADERS, params={
                             **params, 'full': 0}).json()
         return int(data['last'].split('page=')[1])
@@ -134,29 +144,44 @@ def fetch_pages(output_folder, total_pages, url, params, mapping):
         output_folder (str): The folder to save the data.
         url (str): The URL to request.
         params (dict): The parameters to include in the request.
-        page (int): The page number to fetch.
+        total_pages (int): The total number of pages to fetch.
+        mapping (dict): The jq mapping to transform the data.
     """
+    jq_filter_string = '{' + \
+        ', '.join([f'"{key}": {value}' for key,
+                  value in mapping.items()]) + '}'
+    jq_filter = jq.compile(jq_filter_string)
+
     for page in range(total_pages):
         print(f"Fetching page {page} of {total_pages}")
         params['page'] = page
-        data = requests.get(url, headers=HEADERS,
-                            params=params, stream=True).json()
-        with open(os.path.join(output_folder, f"page_{page}.json"), 'w') as f:
-            # Process the data here based on mapping
-            jq_filter = jq.compile(mapping)
-            data['list'] = jq_filter.transform(data['list'])
-            # Save the transformed data to a file
-            f.write(json.dumps(data['list'], indent=4))
+        response = requests.get(url, headers=HEADERS, params=params)
+        data = response.json()
+
+        # Transform the data using the jq filter
+        transformed_data = [jq_filter.transform(
+            item) for item in data.get('list', [])]
+
+        # Save the transformed data to a file
+        output_file = os.path.join(output_folder, f"page_{page}.json")
+        try:
+            with open(output_file, 'w') as f:
+                json.dump(transformed_data, f, indent=None,
+                          separators=(',', ':'))
+            print(f"Saved page {page + 1} to {output_file}")
+        except Exception as e:
+            print(f"Error saving file {output_file}: {e}")
+
 
 def process(output_folder, url, params, mapping):
     """
     Process the data from the Drupal API and save it to a file.
     """
     total_pages = get_total_pages(url, params)
-    if total_pages > 0:
+    if total_pages >= 0:
         fetch_pages(output_folder, total_pages, url, params, mapping)
     return total_pages
-    
+
 
 def main():
     """
@@ -183,32 +208,37 @@ def main():
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
-    # Events.
+    # Define the endpoint and parameters based on the dataset name
     if dataset_name == 'event':
         endpoint = 'node.json'
         params = {'type': 'event', 'sort': 'nid', 'direction': 'ASC'}
-        
-    # user
+
     if dataset_name == 'user':
         endpoint = 'user.json'
         params = {'sort': 'uid', 'direction': "ASC"}
 
-    if dataset_name == "organization":
-        endpoint: 'node.json'
+    if dataset_name == 'organization':
+        endpoint = 'node.json'
         params = {'type': 'organization', 'sort': 'nid', 'direction': 'ASC'}
-        
-    # module
-    # fetch_data "module" "node.json" "type=project_module&sort=nid&direction=ASC" "$module_mapping"
-    # module_terms
-    # fetch_data "module_terms" "taxonomy_term.json" "vocabulary[]=3&vocabulary[]=44&vocabulary[]=46&sort=tid&direction=ASC" "$taxonomy_terms_mapping"
-    # theme
-    # fetch_data "theme" "node.json" "type=project_theme&sort=nid&direction=ASC" "$theme_mapping"
-    # echo "Usage: $0 {user|organization|event|module|module_terms}"
-    
+
+    if dataset_name == 'module':
+        endpoint = 'node.json'
+        params = {'type': 'project_module', 'sort': 'nid', 'direction': 'ASC'}
+
+    if dataset_name == 'module_terms':
+        endpoint = 'taxonomy_term.json'
+        params = {'vocabulary[]': ['3', '44', '46'],
+                  'sort': 'tid', 'direction': 'ASC'}
+
+    if dataset_name == 'theme':
+        endpoint = 'node.json'
+        params = {'type': 'project_theme', 'sort': 'nid', 'direction': 'ASC'}
+
     url = f"{BASE_URL}/{endpoint}"
     total_pages = process(output_folder, url, params, mapping)
-    print(f"Processed {total_pages} pages of data for {dataset_name}")
+    print(f"Processed {total_pages + 1} pages of data for {dataset_name}")
     return
+
 
 if __name__ == "__main__":
     main()
